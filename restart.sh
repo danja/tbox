3c1c397e386967277cb63e2baebbec0334539e29
Danny Ayers <danny.ayers@gmail.com>
2025-02-22 12:55:32 +0100

    tweak - current Fuseki container bug workaround

--- restart.sh
+++ restart.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-chmod -R 777 ./data/fuseki
-docker-compose down
-# --volumes  # Remove volumes - destructive
-docker-compose build --no-cache  # Rebuild without cache
-docker-compose up -d  # Start fresh
 
-docker-compose logs
+# chmod -R 777 ./data/fuseki
+
+sudo systemctl stop tbox
+cd ~/hyperdata/tbox # my local dir
+
+docker-compose down
+
+# docker-compose build --no-cache  # Rebuild without cache
+# --volumes  # Remove volumes - destructive
+
+docker-compose up -d
+
+# docker-compose logs
