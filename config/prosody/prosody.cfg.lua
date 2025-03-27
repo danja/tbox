e4118d1d0de3d43f40ccb97abd0ab2cd59f1f012
Danny Ayers <danny.ayers@gmail.com>
2025-03-27 23:02:47 +0100

    reorg

--- config/prosody/prosody.cfg.lua
+++ config/prosody/prosody.cfg.lua
@@ -1,14 +1,15 @@
-admins = { }
+admins = { "admin@localhost" }
 
 modules_enabled = {
     "roster"; "saslauth"; "tls"; "dialback"; "disco";
     "posix"; "ping"; "register";
     "admin_adhoc"; "offline"; "c2s"; "s2s";
+    "muc"; "muc_mam"; "vcard";
 }
 
 allow_registration = true
-c2s_require_encryption = true
-s2s_require_encryption = true
+c2s_require_encryption = false
+s2s_require_encryption = false
 
 http_monitoring_interval = 30
 
@@ -16,12 +17,18 @@ http_upload_file_size_limit = 104857600
 http_upload_expire_after = 60 * 60 * 24 * 7
 http_upload_path = "/var/lib/prosody/http_upload"
 
-log = "*console"
+log = {
+    debug = "*console";
+}
 
 VirtualHost "localhost"
     ssl = {
         key = "/etc/prosody/certs/localhost.key";
         certificate = "/etc/prosody/certs/localhost.crt";
     }
+
+Component "conference.localhost" "muc"
+    name = "Conference Rooms"
+    restrict_room_creation = false
 
 
--- rebuild-start.sh
+++ rebuild-start.sh
@@ -6,4 +6,4 @@ chmod -R 777 ./data/fuseki
 docker-compose build --no-cache
 rm logs/startup.log
 docker-compose up -d
-docker-compose logs -f > logs/startup.log
+# docker-compose logs -f > logs/startup.log
